263953fd7ce0f33cf74996543fbe969fdd65961b
Kent C. Dodds <kent@doddsfamily.us>
2015-08-11 11:28:53 -0600

    👾 Updating package.json and refactoring Mathy.js

--- common/Mathy.js
+++ common/Mathy.js
@@ -1,15 +1,13 @@
-function sqrt(num){
+export default {sqrt, square, diag};
+
+function sqrt(num) {
   return Math.sqrt(num);
 }
 
-function square(num){
+function square(num) {
   return num * num;
 }
 
-function diag(x, y){
+function diag(x, y) {
   return sqrt(square(x) + square(y));
 }
-
-export default {
-  sqrt,square,diag
-};
