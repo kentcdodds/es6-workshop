422846df4f6c5875db2fd5121e29e71a477d028d
Kent C. Dodds <kent@doddsfamily.us>
2016-10-20 08:37:02 -0400

    add a few more things 2

---
+++ templates/16_es2016.test.js
@@ -0,0 +1,28 @@
+test('the exponentiation operation can be used to raise a number to a power of another number', () => {
+  // WORKSHOP_START
+  // refactor this to use the exponentiation operator (**)
+  const result = Math.pow(3, 2)
+  // WORKSHOP_END
+  // FINAL_START
+  const result = 3**2
+  // FINAL_END
+  expect(result).toBe(9)
+})
+
+test('array.includes can be used to determine whether an item exists in an array', () => {
+  const bestFriend = {name: 'Sindre Sorhus'}
+  const greatFriends = [
+    bestFriend,
+    {name: 'Dustan Kasten'},
+    {name: 'Sam Saccone'},
+    {name: 'Ingvar Stepanyan'},
+  ]
+  // WORKSHOP_START
+  // refactor this use `includes` instead
+  const result = greatFriends.indexOf(bestFriend) !== -1
+  // WORKSHOP_END
+  // FINAL_START
+  const result = greatFriends.includes(bestFriend)
+  // FINAL_END
+  expect(result).toBe(true)
+})
---
+++ templates/17_es2017.test.js
@@ -0,0 +1,156 @@
+import {transform} from 'babel-core'
+
+test('String.prototype.padStart saves us from left-pad-gate', () => {
+  const originalString = 'Worlds Finest'
+  // WORKSHOP_START
+  // call padStart on this string to make the test pass
+  const result = originalString
+  // WORKSHOP_END
+  // FINAL_START
+  const result = originalString.padStart(17)
+  // FINAL_END
+  expect(result).toBe('    Worlds Finest')
+})
+
+test('String.prototype.padEnd (and padStart) can be given a string to pad with', () => {
+  const originalString = 'Stronger Together'
+  // WORKSHOP_START
+  // call padEnd on this string to make the test pass
+  // WORKSHOP_END
+  // FINAL_START
+  const result = originalString.padEnd(27, '-123')
+  // FINAL_END
+  expect(result).toBe('Stronger Together-123-123-1')
+})
+
+test('Object.values gets just the values of an object', () => {
+  const show = {
+    title: 'Supergirl',
+    seasons: 1.2,
+    characters: [
+      'Supergirl',
+      'Cat Grant',
+      'Superman',
+      'Jimmy Olsen',
+      'Hank Henshaw',
+      'Winn Schott',
+      'Alex Danvers',
+    ],
+  }
+  // WORKSHOP_START
+  // get the values of the show object as an array
+  // WORKSHOP_END
+  // FINAL_START
+  const result = Object.values(show)
+  // FINAL_END
+  expect(result).toEqual([
+    'Supergirl',
+    1.2,
+    [
+      'Supergirl',
+      'Cat Grant',
+      'Superman',
+      'Jimmy Olsen',
+      'Hank Henshaw',
+      'Winn Schott',
+      'Alex Danvers',
+    ]
+  ])
+})
+
+test('Object.entries gives an array of arrays as [key, value]', () => {
+  const show = {
+    title: 'The Flash',
+    seasons: 2.2,
+    characters: [
+      'The Flash',
+      'Iris West',
+      'Caitlin Snow',
+      'Eddie Thawne', // 😢
+      'Cisco Ramon',
+      'Harrison Wells',
+      'Joe West',
+    ],
+  }
+  // WORKSHOP_START
+  // get a [key, value] array of the show object
+  // WORKSHOP_END
+  // FINAL_START
+  const result = Object.entries(show)
+  // FINAL_END
+  expect(result).toEqual([
+    [ 'title', 'The Flash' ],
+    [ 'seasons', 2.2 ],
+    [ 'characters', [
+      'The Flash',
+      'Iris West',
+      'Caitlin Snow',
+      'Eddie Thawne',
+      'Cisco Ramon',
+      'Harrison Wells',
+      'Joe West',
+    ]]
+  ])
+})
+
+test('Trailing commas in function parameter lists and calls help us with git', () => {
+  function doStuff() {
+    // use trailing commas everywhere you can...
+    eval(transform(`
+      function foo(
+        a,
+        b,
+        // WORKSHOP_START
+        c
+        // WORKSHOP_END
+        // FINAL_START
+        c,
+        // FINAL_END
+      ) {
+        log(a, b, c)
+      }
+
+      foo(
+        1,
+        2,
+        // WORKSHOP_START
+        3
+        // WORKSHOP_END
+        // FINAL_START
+        3,
+        // FINAL_END
+      )
+
+      function bar(
+        a,
+        b,
+        // WORKSHOP_START
+        ...rest,
+        // WORKSHOP_END
+        // FINAL_START
+        ...rest
+        // FINAL_END
+      ) {
+        log(a, b, ...rest)
+      }
+      bar(
+        1, 2, 3,
+        // WORKSHOP_START
+        4, 5, 6
+        // WORKSHOP_END
+        // FINAL_START
+        4, 5, 6,
+        // FINAL_END
+      )
+
+      function log() {
+        // do nothing :)
+      }
+    `).code)
+  }
+
+  expect(doStuff).not.toThrow()
+})
+
+// there's also this fancy Object.getOwnPropertyDescriptors thing, but you'll likely rarely
+// use that directly... https://github.com/tc39/proposal-object-getownpropertydescriptors
