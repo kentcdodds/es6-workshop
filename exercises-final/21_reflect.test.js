7fc8f71b2ffe7b567ef0945d0f5e8116ef9419c5
Kent C. Dodds <kent@doddsfamily.us>
2017-01-03 16:49:27 -0700

    add reflect stuff

--- exercises-final/21_reflect.test.js
+++ exercises-final/21_reflect.test.js
@@ -23,7 +23,28 @@ test('Reflect.deleteProperty can be used instead of the `delete` keyword', () =>
   expect(person.protected).toBe('YOU CANNOT GET RID OF ME!')
   expect(protectedDeleted).toBe(false)
 })
+
+test(`Reflect.ownKeys returns the object's own (not inherited) keys (including symbols)`, () => {
+  const exists = Symbol('existance')
+  const person = {human: true, [exists]: true}
+  const favoriteFeature = Symbol('Fav Feat')
+  const kyle = {
+    __proto__: person,
+    awesome: true,
+    [favoriteFeature]: 'destructuring',
+  }
+  Reflect.defineProperty(kyle, 'favoriteLanguage', {
+    value: 'JS',
+    configurable: false,
+    enumerable: false,
+  })
+  expect(Object.keys(kyle)).toEqual(['awesome'])
+  expect(Object.getOwnPropertyNames(kyle)).toEqual(['awesome', 'favoriteLanguage'])
+  expect(Object.getOwnPropertySymbols(kyle)).toEqual([favoriteFeature])
+  expect(Reflect.ownKeys(kyle)).toEqual(['awesome', 'favoriteLanguage', favoriteFeature])
+})
 //////// EXTRA CREDIT ////////
 
 // If you get this far, try adding a few more tests, then file a pull request to add them to the extra credit!
 // Learn more here: https://github.com/kentcdodds/es6-workshop/blob/master/CONTRIBUTING.md#development
+// Here's the MDN page for this feature: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Reflect
--- exercises/21_reflect.test.js
+++ exercises/21_reflect.test.js
@@ -17,14 +17,38 @@ test('Reflect.deleteProperty can be used instead of the `delete` keyword', () =>
     value: 'YOU CANNOT GET RID OF ME!',
   })
   // use Reflect.deleteProperty to delete the age property from the person object
-  delete person.age
-  delete person.protected
+  const ageDeleted = delete person.age
+  const protectedDeleted = delete person.protected
   expect(person.age).not.toBeDefined()
   expect(ageDeleted).toBe(true)
   expect(person.protected).toBe('YOU CANNOT GET RID OF ME!')
   expect(protectedDeleted).toBe(false)
 })
+
+test(`Reflect.ownKeys returns the object's own (not inherited) keys (including symbols)`, () => {
+  const exists = Symbol('existance')
+  const person = {human: true, [exists]: true}
+  const favoriteFeature = Symbol('Fav Feat')
+  const kyle = {
+    __proto__: person,
+    awesome: true,
+    [favoriteFeature]: 'destructuring',
+  }
+  Reflect.defineProperty(kyle, 'favoriteLanguage', {
+    value: 'JS',
+    configurable: false,
+    enumerable: false,
+  })
+  // hint, the keys will be in the order that they're added to the object
+  // this will be the case for most environments, though it's generally not
+  // a good idea to rely on this fact as it's not specified in the spec.
+  expect(Object.keys(kyle)).toEqual([/* ENTER YOUR GUESS */])
+  expect(Object.getOwnPropertyNames(kyle)).toEqual([/* ENTER YOUR GUESS */])
+  expect(Object.getOwnPropertySymbols(kyle)).toEqual([/* ENTER YOUR GUESS */])
+  expect(Reflect.ownKeys(kyle)).toEqual([/* ENTER YOUR GUESS */])
+})
 //////// EXTRA CREDIT ////////
 
 // If you get this far, try adding a few more tests, then file a pull request to add them to the extra credit!
 // Learn more here: https://github.com/kentcdodds/es6-workshop/blob/master/CONTRIBUTING.md#development
+// Here's the MDN page for this feature: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Reflect
