365aab2cbbab96f62265cb30d0f9ec5741719395
alayek <alayek@engineer.com>
2017-01-10 01:46:26 +0530

    remove args from quiz functions

--- quizzes/08_spread.js
+++ quizzes/08_spread.js
@@ -1,10 +1,10 @@
 console.log(
-  // getMax([7, 8, 5, 9, 20, 7]),
-  // flatten([8, 9, [6, [5, [7], [45, 34, [[[2]]], [[[[[[[[7]]]]]]]]]]]]),
-  // updateObject({a: 1, b: 3, c: 4}, {b: 5, c: 3, d: 7})
+  // getMax(),
+  // flatten(),
+  // updateObject()
 )
 
-function getMax(arr) {
+function getMax() {
   // TODO: return the maximum value in an array of integers
   // use spread operator and Math.max() in refactoring
   //
@@ -13,28 +13,35 @@ function getMax(arr) {
   // finally returning it
   //
   // A shorter solution is to take advantage of apply()
+  const arr = [5, 6, 8, 4, 9]
   return Math.max.apply(null, arr)
 }
 
-function flatten(arr) {
+function flatten() {
   // TODO: flatten nested arrays of arbitrary levels of nesting
   // arr can be typically like this: [8, 9, [6, [5, [7], [45, 34, [2]]]]]
   // output shold be [8, 9, 6, 5, 7, 45, 34, 2]
   // use spread operator in place of Array.prototype.concat()
-  return arr.reduce((acc, item) => {
-    if (Array.isArray(item)) {
-      return acc.concat(flatten(item))
-    }
-    return acc.concat([item])
-  }, [])
+  const arr = [8, 9, [6, [5, [7], [45, 34, [[[2]]], [[[[[[[[7]]]]], 90]]]]]]]
+  function flatter(arg) {
+    return arg.reduce((acc, item) => {
+      if (Array.isArray(item)) {
+        return acc.concat(flatter(item))
+      }
+      return acc.concat([item])
+    }, [])
+  }
+  return flatter(arr)
 }
 
-function updateObject(obj, updateSpec) {
+function updateObject() {
   // TODO: obj is the original object
   // updateSpec is an object with new values for various fields in obj
   // it might even contain values for fields that don't exist in obj
   // DO NOT alter obj
   // create a new object with updated values and return it
+  const obj = {a: 1, b: 3, c: 4}
+  const updateSpec = {b: 5, c: 3, d: 7}
   return Object.assign({}, obj, updateSpec)
 }
 
@@ -65,17 +72,24 @@ function updateObject(obj, updateSpec) {
 
 
 
-function getMaxSOLUTION(arr) {
+function getMaxSOLUTION() {
+  const arr = [5, 6, 8, 4, 9]
   return Math.max(...arr)
 }
 
-function flattenSOLUTION(arr) {
-  return arr.reduce((acc, item) => {
-    return Array.isArray(item) ? [...acc, ...flatten(item)] : [...acc, item]
-  }, [])
+function flattenSOLUTION() {
+  const arr = [8, 9, [6, [5, [7], [45, 34, [[[2]]], [[[[[[[[7]]]]], 90]]]]]]]
+  function flatter(arg) {
+    return arg.reduce((acc, item) => {
+      return Array.isArray(item) ? [...acc, ...flatter(item)] : [...acc, item]
+    }, [])
+  }
+  return flatter(arr)
 }
 
-function updateObjectSOLUTION(obj, updateSpec) {
+function updateObjectSOLUTION() {
+  const obj = {a: 1, b: 3, c: 4}
+  const updateSpec = {b: 5, c: 3, d: 7}
   return {
     ...obj,
     ...updateSpec,
