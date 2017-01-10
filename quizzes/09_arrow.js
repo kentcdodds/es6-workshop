77825293d64246239078bc7e06e8fe5bc5111df8
Kent C. Dodds <kent@doddsfamily.us>
2017-01-10 11:59:07 -0700

    improve arrow function quizzes

--- quizzes/09_arrow.js
+++ quizzes/09_arrow.js
@@ -1,13 +1,82 @@
 console.log(
+  // multiArgument(),
+  // noArgument(),
+  // singleArgument(),
+  // spreadWithArrow(),
+  // withStatements(),
+  // withObject(),
+  // withMultiLineExpression(),
   // curryAdd(),
-  // genSeries(),
-  // shift(),
-  // getPopulation().display()
+  // context(),
+  // noBind(),
 )
 
+function multiArgument() {
+  // refactor to an arrow function
+  const divide = function(a, b) {
+    return a / b
+  }
+  return divide(40, 10)
+}
+
+function noArgument() {
+  // refactor to an arrow function
+  const getFive = function() {
+    return 5
+  }
+  return getFive()
+}
+
+function singleArgument() {
+  // refactor to an arrow function
+  const identity = function(i) {
+    return i
+  }
+  return identity(350)
+}
+
+function spreadWithArrow() {
+  // refactor to an arrow function
+  const asArray = function(...args) {
+    return args
+  }
+  return asArray(1, 2, 3, 4)
+}
+
+function withStatements() {
+  // refactor to an arrow function
+  const tryInvoke = function(obj, fn, ...args) {
+    try {
+      return obj[fn](...args)
+    } catch (e) {
+      return undefined
+    }
+  }
+  return tryInvoke(Math, 'pow', 2, 3)
+}
+
+function withObject() {
+  // refactor to an arrow function
+  const getObject = function(favoriteCandy) {
+    return {favoriteCandy}
+  }
+  return getObject('twix')
+}
+
+function withMultiLineExpression() {
+  const getString = function(name) {
+    return (
+      `
+        Hello there ${name}
+        How are you doing today?
+      `
+    )
+  }
+  return getString('Ryan')
+}
+
 function curryAdd() {
-  // TODO: refactor the function curryAddition
-  // rewrite with arrow syntax
+  // refactor to a arrow functions
   function curryAddition(a) {
     return function(b) {
       return function(c) {
@@ -18,78 +87,36 @@ function curryAdd() {
   return curryAddition(9)(3)(5)
 }
 
-function genSeries() {
-  const arr = []
-  const n = 8
-  const limit = 100
-
-  // TODO: refactor this function
-  // remove the loop
-  // Use spread opertaor, Array functions in ES6, like Array.from()
-
-  // this function returns an array of n random integers
-  // Each integer can be less than or equal to limit
-  // limit is 10 by default, if not provided
-
-  for (let i = 0; i < n; i++) {
-    arr.push(Math.floor(Math.random() * limit))
-  }
-  return arr
-}
-
-
- // This is a complex example,
- // mostly to show that arrow refactoring
- // doesn't need to hurt readability
- // You may use () => () or () => {}
- // based on how complex you think the code can be
-function shift() {
-  const str = 'aeiou' // str can have punctuations and spaces
-  const amount = 1 // for fun, change it to 26
-
-  return str.replace(/[a-z]/g, function(x) {
-    // first, compute the index of the character
-    // within alphabet
-    // a = 0, b = 1, c = 2,..., z = 25
-    const index = x.charCodeAt(0) - 'a'.charCodeAt()
-    // take modulo, because after adding, we need it to be
-    // in the range [0, 1, ..., 25]
-    const shiftedIndex = (index + amount) % 26
-    // now, convert it back to the shifted character
-    const shiftedCharCode = (shiftedIndex + 'a'.charCodeAt())
-    return String.fromCharCode(shiftedCharCode)
-  })
-}
-
-function getPopulation() {
-  return {
+function context() {
+  const population = {
     unit: 'Million',
     data: [
-      {
-        city: 'Tokyo',
-        value: 37.833,
-      },
-      {
-        city: 'Sanghai',
-        value: 22.991,
-      },
-      {
-        city: 'New Delhi',
-        value: 24.953,
-      },
+      {city: 'Tokyo', value: 37.833},
+      {city: 'Sanghai', value: 22.991},
+      {city: 'New Delhi', value: 24.953},
     ],
-
-    display: function() { // eslint-disable-line func-names
-    // TODO: refactor with arrow function
-    // shorten with method shorthand
-    // Finally, refactor with object destructuring
-      return this.data.map(function(element) { // eslint-disable-line func-names
+    display() {
+      // TODO: refactor with arrow function
+      return this.data.map(function populationStringMapper(element) {
         return `Population in ${element.city} is ${element.value} ${this.unit}`
       }, this) // passing `this` as second argument to bind properly
     },
   }
+  return population.display()
 }
 
+function noBind() {
+  // what will this function return?
+  const user = {
+    id: 42,
+    getIdGetter() {
+      return () => this.id
+    },
+  }
+  const idGetter = user.getIdGetter()
+  const idGetter24 = idGetter.bind({id: 24})
+  return idGetter24()
+}
 
 
 
@@ -104,54 +131,75 @@ function getPopulation() {
 
 
 
+function multiArgumentSOLUTION() {
+  const divide = (a, b) => a / b
+  return divide(40, 10)
+}
 
+function noArgumentSOLUTION() {
+  const getFive = () => 5
+  return getFive()
+}
 
+function singleArgumentSOLUTION() {
+  const identity = i => i
+  return identity(350)
+}
 
+function spreadWithArrowSOLUTION() {
+  // refactor to an arrow function
+  const asArray = (...args) => args
+  return asArray(1, 2, 3, 4)
+}
 
+function withStatementsSOLUTION() {
+  const tryInvoke = (obj, fn, ...args) => {
+    try {
+      return obj[fn](...args)
+    } catch (e) {
+      return undefined
+    }
+  }
+  return tryInvoke(Math, 'pow', 2, 3)
+}
 
+function withObjectSOLUTION() {
+  // refactor to an arrow function
+  const getObject = favoriteCandy => ({favoriteCandy})
+  return getObject('twix')
+}
+
+function withMultiLineExpressionSOLUTION() {
+  const getString = name => (
+    `
+      Hello there ${name}
+      How are you doing today?
+    `
+  )
+  return getString('Ryan')
+}
 
 
 function curryAddSOLUTION() {
-  function curryAddition(a) {
-    return b => c => a + b + c
-  }
+  const curryAddition = a => b => c => a + b + c
   return curryAddition(9)(3)(5)
 }
 
-function genSeriesSOLUTION(n = 10, limit = 10) {
-  return Array.from({length: n}, () => (
-    Math.floor(Math.random() * limit)
-  ))
-}
-
-
-function getPopulationSOLUTION() {
-  return {
+function contextSOLUTION() {
+  const population = {
     unit: 'Million',
     data: [
-      {
-        city: 'Tokyo',
-        value: 37.833,
-      },
-      {
-        city: 'Sanghai',
-        value: 22.991,
-      },
-      {
-        city: 'New Delhi',
-        value: 24.953,
-      },
+      {city: 'Tokyo', value: 37.833},
+      {city: 'Sanghai', value: 22.991},
+      {city: 'New Delhi', value: 24.953},
     ],
-
-    displaySOLUTION() {
-    // TODO: refactor with arrow function
-    // Finally, refactor with object destructuring
-      return this.data.map(({city, value}) => (
-      `Population in ${city} is ${value} ${this.unit}`
-    ))
+    display() {
+      return this.data.map(element => (
+        `Population in ${element.city} is ${element.value} ${this.unit}`
+      ))
     },
   }
+  return population.display()
 }
 
-/* eslint func-names:0 */
-/* eslint prefer-arrow-callback:0 */
+/* eslint func-names:0, func-style:0 */
