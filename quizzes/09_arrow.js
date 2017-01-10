0481c5242905964aae62eb8a2d721467328088cb
alayek <alayek@engineer.com>
2017-01-10 14:28:38 +0530

    remove args from quiz functions

--- quizzes/09_arrow.js
+++ quizzes/09_arrow.js
@@ -1,24 +1,28 @@
 console.log(
-  // curryAdd(2)(4)(6),
+  // curryAdd(),
   // genSeries(),
-  // genSeries(7, 100),
-  // shift("havin pasta for brunch"),
-  // shift("havin pasta for brunch", 13),
-  // shift("univa cnfgn sbe oehapu", 13),
+  // shift(),
   // getPopulation().display()
 )
 
-function curryAdd(a) {
-  // TODO: refactor this function
+function curryAdd() {
+  // TODO: refactor the function curryAddition
   // rewrite with arrow syntax
-  return function(b) { // eslint-disable-line func-names
-    return function(c) { // eslint-disable-line func-names
-      return a + b + c
+  function curryAddition(a) {
+    return function(b) { // eslint-disable-line func-names
+      return function(c) { // eslint-disable-line func-names
+        return a + b + c
+      }
     }
   }
+  return curryAddition(9)(3)(5)
 }
 
-function genSeries(n = 10, limit = 10) {
+function genSeries() {
+  const arr = []
+  const n = 8
+  const limit = 100
+
   // TODO: refactor this function
   // remove the loop
   // Use spread opertaor, Array functions in ES6, like Array.from()
@@ -26,7 +30,6 @@ function genSeries(n = 10, limit = 10) {
   // this function returns an array of n random integers
   // Each integer can be less than or equal to limit
   // limit is 10 by default, if not provided
-  const arr = []
 
   for (let i = 0; i < n; i++) {
     arr.push(Math.floor(Math.random() * limit))
@@ -40,8 +43,11 @@ function genSeries(n = 10, limit = 10) {
  // doesn't need to hurt readability
  // You may use () => () or () => {}
  // based on how complex you think the code can be
-function shift(str, amount = 1) {
-  return str.replace(/[a-z]/g, x => {
+function shift() {
+  const str="aeiou" // str can have punctuations and spaces
+  const amount = 1 // for fun, change it to 26
+
+  return str.replace(/[a-z]/g, function(x){
     // first, compute the index of the character
     // within alphabet
     // a = 0, b = 1, c = 2,..., z = 25
@@ -105,29 +111,17 @@ function getPopulation() {
 
 
 
-function curryAddSOLUTION(a) {
-  return b => c => a + b + c
+function curryAddSOLUTION() {
+  function curryAddition(a){
+    return b => c => a + b + c
+  }
+  return curryAddition(9)(3)(5)
 }
 
 function genSeriesSOLUTION(n = 10, limit = 10) {
   return Array.from({length: n}, () => (
     Math.floor(Math.random() * limit)
   ))
-}
-
-function shiftSOLUTION(str, amount = 1) {
-  return str.replace(/[a-z]/g, x => {
-    // first, compute the index of the character
-    // within alphabet
-    // a = 0, b = 1, c = 2,..., z = 25
-    const index = x.charCodeAt(0) - 'a'.charCodeAt()
-    // take modulo, because after adding, we need it to be
-    // in the range [0, 1, ..., 25]
-    const shiftedIndex = (index + amount) % 26
-    // now, convert it back to the shifted character
-    const shiftedCharCode = (shiftedIndex + 'a'.charCodeAt())
-    return String.fromCharCode(shiftedCharCode)
-  })
 }
 
 
