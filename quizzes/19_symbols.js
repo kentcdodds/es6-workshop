24438f5b33bd152e4fa286b463f827034c70936a
Kent C. Dodds <kent@doddsfamily.us>
2017-01-21 21:05:37 -0700

    add solution to symbol quiz

--- quizzes/19_symbols.js
+++ quizzes/19_symbols.js
@@ -68,6 +68,17 @@ function symbolDescriptionSOLUTION() {
 // log(symbolDescriptionSOLUTION())
 
 
+function getSymbolsFromObjectSOLUTION() {
+  // what will this return?
+  const secret = Symbol('not so secret')
+  const object = {
+    noSecrets: 'I have nothing to hide',
+    [secret]: 'can you find me?',
+  }
+  return Object.getOwnPropertySymbols(object)
+}
+// log(getSymbolsFromObjectSOLUTION())
+
 
 /*
 eslint
