e32342c105b4b4ae6d8f8c8c23c5298ef1978654
Kent C. Dodds <kent@doddsfamily.us>
2017-01-21 20:38:15 -0700

    add public class fields quiz

---
+++ quizzes/18_public-class-fields.js
@@ -0,0 +1,81 @@
+import log from './pretty-log'
+
+function basicExample() {
+  // can you find the bug in this code?
+  // how could we fix it?
+  class Greeter {
+    constructor(name, preference) {
+      this.name = name
+      this.preference = preference
+    }
+    greet(otherName) {
+      return `Hello ${otherName}, my name is ${this.name}`
+    }
+    askAboutWeather() {
+      const antiPreference = this.preference === 'cool' ? 'warm' : 'cool'
+      return `Is it ${antiPreference} outside? I prefer it ${this.preference}.`
+    }
+    getInterestingCallbacks() {
+      return {
+        greet: this.greet,
+        askAboutWeather: this.askAboutWeather,
+      }
+    }
+  }
+  const greeter = new Greeter('Ronald', 'cool')
+  const interestingCallbacks = greeter.getInterestingCallbacks()
+  return {
+    greeting: interestingCallbacks.greet('Jane'),
+    weatherQuestion: interestingCallbacks.askAboutWeather(),
+  }
+}
+// log(basicExample())
+
+
+
+
+
+
+
+
+
+// SOLUTIONS BELOW!
+
+
+
+
+
+
+
+
+
+
+
+function basicExampleSOLUTION() {
+  class Greeter {
+    constructor(name, preference) {
+      this.name = name
+      this.preference = preference
+    }
+    greet = otherName => {
+      return `Hello ${otherName}, my name is ${this.name}`
+    }
+    askAboutWeather = () => {
+      const antiPreference = this.preference === 'cool' ? 'warm' : 'cool'
+      return `Is it ${antiPreference} outside? I prefer it ${this.preference}.`
+    }
+    getInterestingCallbacks() {
+      return {
+        greet: this.greet,
+        askAboutWeather: this.askAboutWeather,
+      }
+    }
+  }
+  const greeter = new Greeter('Ronald', 'cool')
+  const interestingCallbacks = greeter.getInterestingCallbacks()
+  return {
+    greeting: interestingCallbacks.greet('Jane'),
+    weatherQuestion: interestingCallbacks.askAboutWeather(),
+  }
+}
+log(basicExampleSOLUTION())
